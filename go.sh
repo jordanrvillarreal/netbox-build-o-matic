712f2a3a844c44da5232899d7ad4a30c9e3140cb
Jordan Villarreal <37595595+jordanrvillarreal@users.noreply.github.com>
2022-05-23 14:33:42 -0500

    Update and rename go.sh to install.sh
    Renamed to install.sh and updated external script calls

--- go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-chmod u+x install.sh
-chmod u+x bulk.sh
-apt install expect -y
-
-./install.sh
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+chmod u+x step1.sh
+chmod u+x step2.sh
+apt install expect -y
+
+./step1.sh
